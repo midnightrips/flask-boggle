1cdbc11892fe606251b72e660593ae4726eaaa75
Ehretz Geskin <sg3584@alum.barnard.edu>
2024-08-22 10:50:02 -0400

    Debugging

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from boggle import Boggle
 from flask import Flask, render_template, redirect, request, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = '12345'
 app.debug = True
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
@@ -13,19 +15,21 @@ boggle_game = Boggle()
 
 @app.route('/')
 def show_board():
+    """Creates and shows boggle board"""
+
     board = boggle_game.make_board()
     session['BOARD'] = board
     return render_template('board.html', board=board)
 
 
-@app.route('/guess', methods=['POST'])
-def valid_word():
-    guess = request.form.get('guess')
+@app.route('/guess')
+def valid_word(): #checks if word exists and returns JSON
+    """Checks if word exists and returns JSON"""
+
+    guess = request.args['guess']
     board = session['BOARD']
+    print(guess)
 
-    # if guess in boggle_game.words:
     result = boggle_game.check_valid_word(board, guess)
-    info = {"result": result}
-    JSON =  jsonify(info)
 
-    return redirect('/', JSON=JSON)
+    return jsonify({'result': result})
--- static/app.js
+++ static/app.js
@@ -4,8 +4,15 @@ async function sendGuess(e) {
     e.preventDefault();
     let guess = $userInput.val();
     $userInput.val(''); //clear search bar
-    const res = await axios.get('http://localhost', { params: { guess } })
-    console.log(res);
+    const res = await axios.get('http://127.0.0.1:5000/guess', { //
+        params: { guess },
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    console.log(res.data);
 }
 
 $('#submit').on('click', sendGuess);
+
+// const res = await axios.post('http://127.0.0.1:5000/guess', new URLSearchParams({ guess: guess }));
